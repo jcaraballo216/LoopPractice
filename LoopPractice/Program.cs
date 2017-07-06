b3ccd19eafcd3f8db8b8af072f2cb91abbffcfb0
Jerome <jscar216@gmail.com>
2017-07-06 11:15:01 -0400

    For loop examples finished

--- LoopPractice/Program.cs
+++ LoopPractice/Program.cs
@@ -23,7 +23,7 @@ namespace LoopPractice
             //{
             //do something
             //}
-            
+
             //for(int i = 15; i > 0; i--)
             //{
             //    Console.WriteLine(i);//counts down from 15 to 1 
@@ -33,6 +33,52 @@ namespace LoopPractice
             //    Console.WriteLine(i);//counts up from 50 to 100
             //}
 
+            //string greeting = "My name is little Bill";
+            //The .Split() method takes a string and turns it into an array of smaller..
+            //strings (substrings). It defaults the split on spaces.
+            //string[] wordsInGreeting = greeting.Split();
+            ////for loop to print the words
+            //for (int i = 0; i < wordsInGreeting.Length; i++)
+            //{
+            //    Console.WriteLine(wordsInGreeting[i]);//prints out the words
+            //}
+            //Create an array called days with the elements Monday, Tuesday, and Wednesday.
+            //Using a For loop, print the elements of the days array.
+
+            //Start with the string "Once upon a time" and crate an array called...
+            //storyWords using the Split Method, Reverse the order of the elements in..
+            //the array and using a for loop, print each word.
+
+            //Add the product (multiplication) of 1 through 10 using a for loop
+            //Print product to screen.
+
+            //string[] days = { "Monday", "Tuesday", "Wednesday" };
+            //    for(int i = 0; i<=2; i++)
+            //    {
+            //    Console.WriteLine(days[i]);
+            //    }
+
+
+            //string message = "Once upon a time";
+            //string[] storyWords = message.Split();
+            //Array.Reverse(storyWords);
+            //for (int i = 0; i < storyWords.Length; i++)
+            //{
+            //    Console.WriteLine(storyWords[i]);
+            //}
+
+            int product = 1;
+                for(int i = 1; i<=10; i++)
+            {
+                product = product * i;
+            }
+            Console.WriteLine(product);
+
+
+
+            
+            
+            
 
         }
     }
